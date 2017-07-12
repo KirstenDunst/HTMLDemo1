1830ef9aa3687195cc4eacfea8fe47fe50a749b6
KirstenDunst <cao_shixin@yahoo.com>
2017-07-12 10:21:49 +0800

    反射第一次课

---
+++ 反射/Runmou.Dal/SelserverHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Runmou.Dal
+{
+    public class SelserverHelper
+    {
+
+        public SelserverHelper(){
+            Console.WriteLine("这里开始构建SelserverHelper");
+        }
+
+
+        public void Query(){
+            Console.WriteLine("SelserverHelper Query");
+        }
+    }
+}
---
+++ 反射/反射/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+//using Runmou.Dal;
+
+//这个是dotnet提供给我们的一个反射的类库（可以帮助我们动态的解析dal，动态的使用），今天是我们使用一下动态的加载
+using System.Reflection;
+
+namespace 反射
+{
+
+	//反射的原理是基于 metadata（元数据）
+
+	class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+			//         //普通做法（引入类库，创建对象调用方法）
+			////这里我们在使用SqlserverHelper类的时候会又有提示（需要我们复制来SqlserverHelper类名之后右键resolve一下）
+			//SelserverHelper sqlserverHelper = new SelserverHelper();
+            //sqlserverHelper.Query();
+
+
+
+
+
+			//反射
+			//1.需要把Runmou.Dal文件夹内部的bin->Debug->里面的Runmou.Dal.pdb和Runmou.Dal.dll两个文件复制到反射->bin->Debug->里面
+
+			//2.动态加载dll(如果这里报错，留意检查第一步的文件重新编译的时候给去除了)
+			Assembly assembly = Assembly.Load("Runmou.Dal");
+
+			Console.WriteLine("\n**************************Module模块的**************************\n");
+			//获取他的所有的模块
+			Module[] modules = assembly.GetModules();
+			foreach (Module module in modules)
+			{
+				Console.WriteLine(module.Name);
+			}
+
+
+			//Console.WriteLine("\n**************************Type 类**************************\n");
+			////获取他的模块
+			//Type[] types = assembly.GetTypes();
+			//foreach (Type type in types)
+			//{
+			//	Console.WriteLine(type.Name);
+			//}
+			////根据关键字找到具体的某一个类
+			//Type typeHelper = assembly.GetType("Runmou.Dal.SqlserverHelper");
+			////初始化SqlserverHelper的对象
+			//Object oHelper = Activator.CreateInstance(typeHelper);
+
+
+
+
+
+			//保证控制台不会跳走
+			Console.Read();
+        }
+    }
+}
