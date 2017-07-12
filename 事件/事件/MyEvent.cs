142e4203cafbae4e6c3da6e9361783d9223a8ebc
KirstenDunst <cao_shixin@yahoo.com>
2017-07-12 16:59:32 +0800

    //委托是一种类型         //事件是委托的一个实例

---
+++ 事件/事件/MyEvent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace 事件
+{
+
+    //
+    public delegate void DoSomething();
+
+    public class MyEvent
+    {
+        //委托的一个变量
+        public  DoSomething DoSomethingHandlerDelegate;
+        //事件：就是一个委托的变量，然后再加上了关键字event修饰
+        public event DoSomething DoSomethingHandler;
+
+
+        //委托是一种类型
+        //事件是委托的一个实例
+
+
+        //People就是一种类型 而Eleven就是people的一个实例
+        private People people = new People()
+        {
+            Id = 11,
+            Name = "Eleven"
+        };
+
+
+
+        public  void  Invoke(){
+            if(DoSomethingHandler != null){
+                DoSomethingHandler.Invoke();
+            }
+        }
+
+
+
+    }
+
+
+
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 事件/事件/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 事件
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("事件Event的使用！");
+
+
+            MyEvent myEvent = new MyEvent();
+            myEvent.DoSomethingHandler += new DoSomething(ShowSomething);
+            myEvent.DoSomethingHandler += ShowSomething;
+            myEvent.DoSomethingHandler += ShowSomething;
+
+            myEvent.DoSomethingHandler -= ShowSomething;
+            myEvent.Invoke();
+
+
+
+            Console.Read();
+        }
+
+        private static void ShowSomething(){
+            Console.WriteLine("ShowSomething");
+        }
+    }
+}
