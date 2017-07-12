6bd2d2fbaf57843a2cb3f832f52e72f976f4255a
KirstenDunst <cao_shixin@yahoo.com>
2017-07-12 13:58:16 +0800

    反射第三次课

---
+++ 反射/Runmou.Dal.Mysql/MysqlHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Runmou.Interface;
+
+namespace Runmou.Dal.Mysql
+{
+	public class  MysqlHelper : IHelper
+	{
+
+		public MysqlHelper()
+		{
+			Console.WriteLine("这里开始构建SelserverHelper");
+		}
+
+
+		public void Query()
+		{
+			Console.WriteLine("SelserverHelper Query");
+		}
+	}
+}
--- 反射/反射/Program.cs
+++ 反射/反射/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿﻿using System;
 //using Runmou.Dal;
 
 //这个是dotnet提供给我们的一个反射的类库（可以帮助我们动态的解析dal，动态的使用），今天是我们使用一下动态的加载
 using System.Reflection;
 using Runmou.Interface;
+using System.Configuration;
 
 namespace 反射
 {
@@ -25,38 +26,59 @@ namespace 反射
 
 
 
-			//反射
-			//1.需要把Runmou.Dal文件夹内部的bin->Debug->里面的Runmou.Dal.pdb和Runmou.Dal.dll两个文件复制到反射->bin->Debug->里面
+			////反射
+			////1.需要把Runmou.Dal文件夹内部的bin->Debug->里面的Runmou.Dal.pdb和Runmou.Dal.dll两个文件复制到反射->bin->Debug->里面
 
-			//2.动态加载dll(如果这里报错，留意检查第一步的文件重新编译的时候给去除了)
-			Assembly assembly = Assembly.Load("Runmou.Dal");
+			////2.动态加载dll(如果这里报错，留意检查第一步的文件重新编译的时候给去除了)
+			//Assembly assembly = Assembly.Load("Runmou.Dal");
 
-			Console.WriteLine("\n**************************Module模块的**************************\n");
-			//获取他的所有的模块
-			Module[] modules = assembly.GetModules();
-			foreach (Module module in modules)
-			{
-				Console.WriteLine(module.Name);
-			}
+			//Console.WriteLine("\n**************************Module模块的**************************\n");
+			////获取他的所有的模块
+			//Module[] modules = assembly.GetModules();
+			//foreach (Module module in modules)
+			//{
+			//	Console.WriteLine(module.Name);
+			//}
 
-
-			Console.WriteLine("\n**************************Type 类**************************\n");
-			//获取他的所有类型
-			Type[] types = assembly.GetTypes();
-			foreach (Type type in types)
-			{
-				Console.WriteLine(type.Name);
-			}
-			//根据关键字找到具体的某一个类
-			Type typeHelper = assembly.GetType("Runmou.Dal.SelserverHelper");
-            //初始化SqlserverHelper的对象(反射的方法创建一个对象)
-			Object oHelper = Activator.CreateInstance(typeHelper);
-
-            IHelper iHelper = (IHelper)oHelper;
-            iHelper.Query();
-
+			//Console.WriteLine("\n**************************Type 类**************************\n");
+			////获取他的所有类型
+			//Type[] types = assembly.GetTypes();
+			//foreach (Type type in types)
+			//{
+			//	Console.WriteLine(type.Name);
+			//}
+			////根据关键字找到具体的某一个类
+			//Type typeHelper = assembly.GetType("Runmou.Dal.SelserverHelper");
+   //         //初始化SqlserverHelper的对象(反射的方法创建一个对象)
+			//Object oHelper = Activator.CreateInstance(typeHelper);
+            ////通过接口管理的方式调取方法
+            //IHelper iHelper = (IHelper)oHelper;
+            //iHelper.Query();
 
 
+
+
+
+
+            Console.WriteLine("\n**************************可配置可扩展的**************************\n");
+            //第一步，需要在App.config的配置文件里面添加配置：
+            /*<appSettings>
+			 * <add key = "IHelperConfig" value="Runmou.Dal,Runmou.Dal.SelserverHelper" />
+             * </appSettings>
+             */
+            //第二步：读取文件。添加系统的一个引用类System.Configuration
+            //第三步:读取
+            string IHelperConfig = ConfigurationManager.AppSettings["IHelperConfig"];
+			//第四步：动态加载dll
+            Assembly assembly = Assembly.Load(IHelperConfig.Split(',')[0]);
+            Type typeHelper = assembly.GetType(IHelperConfig.Split(',')[1]);//找出具体类型
+			Object oHelper = Activator.CreateInstance(typeHelper);//创建对象
+			IHelper iHelper = (IHelper)oHelper;//转换对象
+			iHelper.Query();//调用方法
+            /*我们在需要替换的时候只需要修改配置环境（App.config）里面的东西,（前提要记得把编译生成的dll文件拷贝到这个文件路径下）
+             * 比如使用新建一个Runmou.Dal.Mysql新类的时候，我们只需要修改为
+             * <add key = "IHelperConfig" value="Runmou.Dal.Mysql,Runmou.Dal.Mysql.MysqlHelper" />
+             */
 
 			//保证控制台不会跳走
 			Console.Read();
