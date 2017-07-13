5ec8d324d41c07ef3ccd285205858bae8e030fdd
KirstenDunst <cao_shixin@yahoo.com>
2017-07-13 11:07:55 +0800

    反射的程序配置文件，添加文件选择Misc-Application Configuration File

---
+++ 异步/异步/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 异步
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            Console.Read();
+        }
+    }
+}
